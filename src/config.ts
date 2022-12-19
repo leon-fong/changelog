d71e49eead1b71e4de2611e3766f8307b3833797
fanglm <ooohmydawn@hotmail.com>
2022-12-19 15:52:17 +0800

    fix: remove emoji

--- src/config.ts
+++ src/config.ts
@@ -17,18 +17,18 @@ export interface ChangelogConfig {
 
 const ConfigDefaults: ChangelogConfig = {
   types: {
-    feat: { title: '🚀 Enhancements', semver: 'minor' },
-    perf: { title: '🔥 Performance', semver: 'patch' },
-    fix: { title: '🩹 Fixes', semver: 'patch' },
-    refactor: { title: '💅 Refactors', semver: 'patch' },
-    docs: { title: '📖 Documentation', semver: 'patch' },
-    build: { title: '📦 Build', semver: 'patch' },
-    types: { title: '🌊 Types', semver: 'patch' },
-    chore: { title: '🏡 Chore' },
-    examples: { title: '🏀 Examples' },
-    test: { title: '✅ Tests' },
-    style: { title: '🎨 Styles' },
-    ci: { title: '🤖 CI' }
+    feat: { title: 'Features', semver: 'minor' },
+    perf: { title: 'Performance', semver: 'patch' },
+    fix: { title: 'Fixes', semver: 'patch' },
+    refactor: { title: 'Refactors', semver: 'patch' },
+    docs: { title: 'Documentation', semver: 'patch' },
+    build: { title: 'Build', semver: 'patch' },
+    types: { title: 'Types', semver: 'patch' },
+    chore: { title: 'Chore' },
+    examples: { title: 'Examples' },
+    test: { title: 'Tests' },
+    style: { title: 'Styles' },
+    ci: { title: 'CI' }
   },
   cwd: null,
   github: '',
